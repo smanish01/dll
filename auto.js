7e79632f9fbac3b60e009c60bfdce0af43f6214c
smanish01 <manish.shukla@built.io>
2018-07-25 12:32:47 +0530

    ok

---
+++ auto.js
@@ -0,0 +1,170 @@
+var obj1 = {
+    "repository_html_description": "",
+    "repository_html_fork": "",
+    "repository_url": "",
+    "repository_forks_url": "",
+    "repository_keys_url": "",
+    "repository_collaborators_url": "",
+    "repository_teams_url": "",
+    "repository_hooks_url": "",
+    "repository_issue_events_url": "",
+    "repository_events_url": "",
+    "repository_assignees_url": "",
+    "repository_branches_url": "",
+    "repository_tags_url": "",
+    "repository_blobs_url": "",
+    "repository_git_tags_url": "",
+    "repository_git_refs_url": "",
+    "repository_trees_url": "",
+    "repository_statuses_url": "",
+    "repository_languages_url": "",
+    "repository_stargazers_url": "",
+    "repository_contributors_url": "",
+    "repository_subscribers_url": "",
+    "repository_subscription_url": "",
+    "repository_commits_url": "",
+    "repository_git_commits_url": "",
+    "repository_comments_url": "",
+    "repository_issue_comment_url": "",
+    "repository_contents_url": "",
+    "repository_compare_url": "",
+    "repository_merges_url": "",
+    "repository_archive_url": "",
+    "repository_downloads_url": "",
+    "repository_issues_url": "",
+    "repository_pulls_url": "",
+    "repository_milestones_url": "",
+    "repository_notifications_url": "",
+    "repository_labels_url": "",
+    "repository_releases_url": "",
+    "repository_deployments_url": "",
+    "repository_created_at": "",
+    "repository_updated_at": "",
+    "repository_pushed_at": "",
+    "repository_git_url": "",
+    "repository_ssh_url": "",
+    "repository_clone_url": "",
+    "repository_svn_url": "",
+    "repository_homepage": "",
+    "repository_size": "",
+    "repository_stargazers_count": "",
+    "repository_watchers_count": "",
+    "repository_language": "",
+    "repository_has_issues": "",
+    "repository_has_projects": "",
+    "repository_has_downloads": "",
+    "repository_has_wiki": "",
+    "repository_has_pages": "",
+    "repository_forks_count": "",
+    "repository_mirror_url": "",
+    "repository_archived": "",
+    "repository_open_issues_count": "",
+    "repository_license": "",
+    "repository_forks": "",
+    "repository_open_issues": "",
+    "repository_watchers": "",
+    "repository_default_branch": "",
+    "repository_stargazers": "",
+    "repository_master_branch": "",
+}
+
+var obj2 = JSON.parse(JSON.stringify(obj1))
+
+
+// console.log(JSON.stringify(obj2))
+
+var obj3 = {
+    "description": "doubly linked list javascript class",
+    "fork": false,
+    "url": "https://github.com/john/repo",
+    "forks_url": "https://api.github.com/repos/john/repo/forks",
+    "keys_url": "https://api.github.com/repos/john/repo/keys{/key_id}",
+    "collaborators_url": "https://api.github.com/repos/john/repo/collaborators{/collaborator}",
+    "teams_url": "https://api.github.com/repos/john/repo/teams",
+    "hooks_url": "https://api.github.com/repos/john/repo/hooks",
+    "issue_events_url": "https://api.github.com/repos/john/repo/issues/events{/number}",
+    "events_url": "https://api.github.com/repos/john/repo/events",
+    "assignees_url": "https://api.github.com/repos/john/repo/assignees{/user}",
+    "branches_url": "https://api.github.com/repos/john/repo/branches{/branch}",
+    "tags_url": "https://api.github.com/repos/john/repo/tags",
+    "blobs_url": "https://api.github.com/repos/john/repo/git/blobs{/sha}",
+    "git_tags_url": "https://api.github.com/repos/john/repo/git/tags{/sha}",
+    "git_refs_url": "https://api.github.com/repos/john/repo/git/refs{/sha}",
+    "trees_url": "https://api.github.com/repos/john/repo/git/trees{/sha}",
+    "statuses_url": "https://api.github.com/repos/john/repo/statuses/{sha}",
+    "languages_url": "https://api.github.com/repos/john/repo/languages",
+    "stargazers_url": "https://api.github.com/repos/john/repo/stargazers",
+    "contributors_url": "https://api.github.com/repos/john/repo/contributors",
+    "subscribers_url": "https://api.github.com/repos/john/repo/subscribers",
+    "subscription_url": "https://api.github.com/repos/john/repo/subscription",
+    "commits_url": "https://api.github.com/repos/john/repo/commits{/sha}",
+    "git_commits_url": "https://api.github.com/repos/john/repo/git/commits{/sha}",
+    "comments_url": "https://api.github.com/repos/john/repo/comments{/number}",
+    "issue_comment_url": "https://api.github.com/repos/john/repo/issues/comments{/number}",
+    "contents_url": "https://api.github.com/repos/john/repo/contents/{+path}",
+    "compare_url": "https://api.github.com/repos/john/repo/compare/{base}...{head}",
+    "merges_url": "https://api.github.com/repos/john/repo/merges",
+    "archive_url": "https://api.github.com/repos/john/repo/{archive_format}{/ref}",
+    "downloads_url": "https://api.github.com/repos/john/repo/downloads",
+    "issues_url": "https://api.github.com/repos/john/repo/issues{/number}",
+    "pulls_url": "https://api.github.com/repos/john/repo/pulls{/number}",
+    "milestones_url": "https://api.github.com/repos/john/repo/milestones{/number}",
+    "notifications_url": "https://api.github.com/repos/john/repo/notifications{?since,all,participating}",
+    "labels_url": "https://api.github.com/repos/john/repo/labels{/name}",
+    "releases_url": "https://api.github.com/repos/john/repo/releases{/id}",
+    "deployments_url": "https://api.github.com/repos/john/repo/deployments",
+    "created_at": 1525553246,
+    "updated_at": "2016-07-24T12:13:53Z",
+    "pushed_at": 1532466526,
+    "git_url": "git://github.com/john/repo.git",
+    "ssh_url": "git@github.com:john/repo.git",
+    "clone_url": "https://github.com/john/repo.git",
+    "svn_url": "https://github.com/john/repo",
+    "homepage": null,
+    "size": 7,
+    "stargazers_count": 0,
+    "watchers_count": 0,
+    "language": "JavaScript",
+    "has_issues": true,
+    "has_projects": true,
+    "has_downloads": true,
+    "has_wiki": true,
+    "has_pages": false,
+    "forks_count": 1,
+    "mirror_url": null,
+    "archived": false,
+    "open_issues_count": 1,
+    "license": null,
+    "forks": 1,
+    "open_issues": 1,
+    "watchers": 0,
+    "default_branch": "master",
+    "stargazers": 0,
+    "master_branch": "master"
+}
+
+var obj4 = JSON.parse(JSON.stringify(obj3))
+
+var arrOfObj4 = []
+
+for (var property2 in obj4)
+{
+    arrOfObj4.push(obj4[property2])
+}
+
+// console.log(arrOfObj4)
+
+var counter = 0
+
+for (var property1 in obj2) {
+    obj2[property1] = arrOfObj4[counter]
+    counter++
+}
+
+console.log(JSON.stringify(obj2))
+
+
+
+
+
+
